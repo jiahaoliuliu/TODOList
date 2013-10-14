7ab0a8420c34b795a402307156b00330df5e947d
Jiahao Liu <jliu@gowex.com>
2013-10-14 12:31:48 +0200

    The first version using supportFragment and SupportFragmentManager.

--- src/com/jiahaoliuliu/todolist/MainActivity.java
+++ src/com/jiahaoliuliu/todolist/MainActivity.java
@@ -1,22 +1,39 @@
 package com.jiahaoliuliu.todolist;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements OnNewItemAddedListener {
 
+	private ArrayAdapter<String> aa;
+	private ArrayList<String> todoItems;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Get reference to the Fragments
+		FragmentManager fm = getSupportFragmentManager();
+		ToDoListFragment todoListFragment =
+				(ToDoListFragment)fm.findFragmentById(R.id.ToDoListFragment);
+		
+		// Create the array list of to do items
+		todoItems = new ArrayList<String>();
+		
+		// Create the array adapter to bind the array to the listview
+		aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+		
+		// Bind the array adapter to the listview.
+		todoListFragment.setListAdapter(aa);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onNewItemAdded(String newItem) {
+		todoItems.add(newItem);
+		aa.notifyDataSetChanged();
 	}
-
 }
---
+++ src/com/jiahaoliuliu/todolist/ToDoListFragment.java
@@ -0,0 +1,6 @@
+package com.jiahaoliuliu.todolist;
+
+import android.support.v4.app.ListFragment;
+
+public class ToDoListFragment extends ListFragment {
+}
